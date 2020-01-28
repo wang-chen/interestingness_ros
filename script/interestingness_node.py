5f2ef99ed0f7861474b3752490c85bfc6c49be9f
Chen Wang <jeffsan@gmx.com>
2020-01-27 21:26:34 -0500

    add msg and interestingness works

--- script/interestingness_node.py
+++ script/interestingness_node.py
@@ -36,17 +36,23 @@ import torch
 import rospy
 import rospkg
 import argparse
+import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import torchvision.transforms as transforms
-from torchvision.transforms.functional import vflip
 
 rospack = rospkg.RosPack()
 pack_path = rospack.get_path('interestingness_ros')
-sys.path.append(pack_path)
 interestingness_path = os.path.join(pack_path,'interestingness')
+sys.path.append(pack_path)
 sys.path.append(interestingness_path)
-from interestingness import *
+
+from interestingness_ros.msg import InterestInfo
+from interestingness.test_interest import Interest, MovAvg, show_batch_box
+from interestingness.interestingness import AE, VAE, AutoEncoder, Interestingness
+from interestingness.dataset import ImageData, Dronefilm, DroneFilming, SubT, SubTF, PersonalVideo
+from interestingness.torchutil import VerticalFlip, count_parameters, show_batch, show_batch_origin, Timer, MovAvg
+from interestingness.torchutil import ConvLoss, CosineLoss, CorrelationLoss, Split2d, Merge2d, PearsonLoss, FiveSplit2d
 
 
 class InterestNode:
@@ -54,26 +60,42 @@ class InterestNode:
         super(InterestNode, self).__init__()
         rospy.init_node('interestingness_node')
         rospy.Subscriber(args.image_topic, Image, self.callback)
+        self.image_pub = rospy.Publisher('interestingness_image', Image)
+        self.info_pub = rospy.Publisher('interestingness_info', InterestInfo)
+
         self.transform = transform
         self.bridge = CvBridge()
-        # self.net = torch.load(args.model_save)
-        # self.net.set_train(False)
-        # self.net.memory.set_learning_rate(rr=args.rr, wr=args.wr)
+        self.net = torch.load(args.model_save)
+        self.net.set_train(False)
+        self.net.memory.set_learning_rate(rr=args.rr, wr=args.wr)
+        if torch.cuda.is_available():
+            self.net = self.net.cuda()
+        torch.cuda.synchronize()
 
     def spin(self):
         rospy.spin()
 
     def callback(self, msg):
+        if msg.header.seq % args.skip_frames is not 0:
+            return
         rospy.loginfo("Received image: %d"%(msg.header.seq))
         try:
             frame = self.bridge.imgmsg_to_cv2(msg, "rgb8")
-            frame = vflip(PIL.Image.fromarray(frame))
-            frame = self.transform(frame)
+            frame = PIL.Image.fromarray(frame)
+            frame = self.transform(frame).unsqueeze(dim=0)
         except CvBridgeError:
             rospy.logerr(CvBridgeError)
         else:
             frame = frame.cuda() if torch.cuda.is_available() else frame
-            show_batch(frame)
+            _, loss = self.net(frame)
+            loss = movavg.append(loss)
+            frame = 255*show_batch_box(frame, msg.header.seq, loss.item(),show_now=False)
+            img_msg = self.bridge.cv2_to_imgmsg(frame.astype(np.uint8))
+            info = InterestInfo()
+            info.level = loss.item()
+            info.header = img_msg.header = msg.header
+            self.image_pub.publish(img_msg)
+            self.info_pub.publish(info)
 
 
 if __name__ == '__main__':
@@ -82,9 +104,9 @@ if __name__ == '__main__':
     parser.add_argument("--model-save", type=str, default=pack_path+'/saves/ae.pt.SubTF.interest.mse', help="read model")
     parser.add_argument("--crop-size", type=int, default=320, help='crop size')
     parser.add_argument("--num-interest", type=int, default=10, help='loss compute by grid')
-    parser.add_argument("--skip-frames", type=int, default=1, help='number of skip frame')
+    parser.add_argument("--skip-frames", type=int, default=5, help='number of skip frame')
     parser.add_argument("--window-size", type=int, default=1, help='smooth window size >=1')
-    parser.add_argument('--save-flag', type=str, default='interests', help='save name flag')
+    parser.add_argument('--save-flag', type=str, default='ros-interest', help='save name flag')
     parser.add_argument("--rr", type=float, default=5, help="reading rate")
     parser.add_argument("--wr", type=float, default=5, help="writing rate")
     args = parser.parse_args(); print(args)
@@ -94,13 +116,15 @@ if __name__ == '__main__':
         os.makedirs(results_path)
 
     transform = transforms.Compose([
+        VerticalFlip(), # Front camera of SubTF is mounted vertical flipped.
         transforms.CenterCrop(args.crop_size),
         transforms.Resize((args.crop_size,args.crop_size)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-    interest = Interest(args.num_interest, os.path.join(results_path, '%s.txt'%('test_name')))
+    drawbox = ConvLoss(input_size=args.crop_size, kernel_size=args.crop_size//2, stride=args.crop_size//4)
+
+    interest = Interest(args.num_interest, os.path.join(results_path, '%s.txt'%(args.save_flag)))
 
     movavg = MovAvg(args.window_size)
 
